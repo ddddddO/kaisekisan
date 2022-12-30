397a78fc303cb9a2e6740c4d637ec85dea633674
ddddddO <lbfdeatq@gmail.com>
2022-12-30 15:52:56 +0900

    modify

---
+++ filter.go
@@ -0,0 +1,108 @@
+package kaisekisan
+
+import "strings"
+
+type filter struct {
+	origin   string
+	pos      []string
+	filtered string
+}
+
+func (f *filter) String() string {
+	f = f.first().second()
+	return f.filtered
+}
+
+func (f *filter) first() *filter {
+	filtered := ""
+	ippan := 0
+	sei := 0
+	for i := range f.pos {
+		filtered = f.pos[i]
+		if filtered == "一般" {
+			ippan = i
+		}
+		if filtered == "姓" {
+			sei = i
+		}
+		if filtered == "*" {
+			filtered = f.pos[i-1]
+			break
+		}
+	}
+	if 2 <= ippan {
+		f.filtered = f.pos[ippan-1]
+		return f
+	}
+	if 1 <= sei {
+		f.filtered = f.pos[sei-1]
+		return f
+	}
+	f.filtered = filtered
+	return f
+}
+
+func (f *filter) second() *filter {
+	if f.filtered == "数" {
+		if f.isPhoneNumber() {
+			f.filtered = "電話番号"
+			return f
+		}
+		if f.isPostCode() {
+			f.filtered = "郵便番号"
+			return f
+		}
+		return f
+	}
+
+	if f.filtered == "一般" {
+		if f.isID() {
+			f.filtered = "ID"
+			return f
+		}
+		return f
+	}
+
+	return f
+}
+
+// 以下、filtered structみたいなのを作って、そっちにメソッドはやしたい。Stringメソッドも定義しておけば楽
+
+// TODO: ちょっと広すぎるかも...
+func (f *filter) isID() bool {
+	return strings.Contains(f.origin, `-`)
+}
+
+// TODO:
+func (f *filter) isPhoneNumber() bool {
+	if f.isMobilePhoneNumber() {
+		return true
+	}
+	if f.isShigaikyokuban() {
+		return true
+	}
+	return false
+}
+
+var mobilePhonePrefixies = []string{"070", "080", "090"}
+
+func (f *filter) isMobilePhoneNumber() bool {
+	for i := range mobilePhonePrefixies {
+		if strings.HasPrefix(f.origin, mobilePhonePrefixies[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+// TODO: 03-xxxxなど
+func (f *filter) isShigaikyokuban() bool {
+	_ = f.origin
+	return false
+}
+
+// TODO: 多分他のAPI使わせてもらうかな...レートリミットとかAPI KEY必要とか心配
+func (f *filter) isPostCode() bool {
+	_ = f.origin
+	return false
+}
